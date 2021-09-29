b1e7d205ec327e8489d1a83f5cf3fb359ac933ae
surafelAndualem <89804317+surafelAndualem@users.noreply.github.com>
2021-09-28 20:57:31 -0500

    Delete day2/day2-hw3/hw-3 directory

--- day2/day2-hw3/hw-3/app3.js
+++
@@ -1,5 +0,0 @@
-const child_process= require("child_process");
-console.log("1: Start");
-const newProcess= child_process.spawn("node",
-["computation/fibonacci.js"], {stdio : "inherit"});
-console.log("2: End");
